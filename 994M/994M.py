3c5d6b852c8c2327894983d67c96a48b3897f50b
arianacai <xc984@nyu.edu>
2020-08-24 12:34:23 +0800

    20200824

--- 994M/994M.py
+++ 994M/994M.py
@@ -1,21 +1,20 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
-        rotten = False
+        rotten = False  # if there is at least one fresh orange
         q = []
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 2:
-                    q.append([i, j, 0])
+                    q.append([i, j, 0])  # the last one is the time
                 elif grid[i][j] == 1:
                     rotten = True
-        if not rotten:
+        if not rotten:  # all cells are 2 or 0
             return 0
-        if not q:
+        if not q:   # no rotten orange. The grid has no change.
             return -1
         while q:
             [a, b, c] = q.pop(0)
-
             if a > 0 and grid[a - 1][b] == 1:
                 grid[a - 1][b] = 2
                 q.append([a - 1, b, c + 1])
@@ -25,13 +24,12 @@ class Solution:
             if b > 0 and grid[a][b - 1] == 1:
                 grid[a][b - 1] = 2
                 q.append([a, b - 1, c + 1])
-            if b < col - 1 and graid[a][b + 1] == 1:
+            if b < col - 1 and grid[a][b + 1] == 1:
                 grid[a][b + 1] = 2
                 q.append([a, b + 1, c + 1])
-                print(4)
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == 1:
+                if grid[i][j] == 1:  # if there is still fresh orange.
                     return -1
         return c
 
