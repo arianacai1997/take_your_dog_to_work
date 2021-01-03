104ffebe3f1de4f047e939c5dcb76ea769aeb2f6
caixiyan <xc984@nyu.edu>
2021-01-03 16:02:07 +0800

    209M. Course Schedule

---
+++ 209M. Course Schedule/209M.py
@@ -0,0 +1,21 @@
+class Solution:
+    # use a dfs to record visited and test if there is a circle: TLE
+    # topological sort
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = {u: [] for u in range(numCourses)}
+        in_degree = [0] * numCourses
+        for u, v in prerequisites:
+            in_degree[u] += 1
+            g[v].append(u)
+        start = [u for u in range(numCourses) if in_degree[u] == 0]
+        # result = []
+        cnt = 0
+        while start:
+            u = start.pop()
+            # result.append(u)
+            cnt += 1
+            for v in g[u]:
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    start.append(v)
+        return True if cnt == numCourses else False
